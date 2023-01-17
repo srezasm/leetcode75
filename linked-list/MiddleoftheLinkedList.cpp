d8f3e80476d0f9b4577764703f1d4b983f3c96ee
srezasm <srezas.2017@gmail.com>
2023-01-17 13:24:54 +0330

    linked list

--- linked-list/MiddleoftheLinkedList.cpp
+++ linked-list/MiddleoftheLinkedList.cpp
@@ -1,5 +1,40 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* ptr = head;
+        int len = 0;
+        while (ptr->next != NULL) {
+            len++;
+            ptr = ptr->next;
+        }
+        len = ceil((len + 1) / 2);
+        for (int i = 0; i < len; i++) {
+            head = head->next;
+        }
+        return head;
+    }
+};
+
+// class Solution {
+//    public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode* twostep = head;
+//         ListNode* onestep = head;
+//         while (twostep != NULL && twostep->next != NULL) {
+//             twostep = twostep->next->next;
+//             onestep = onestep->next;
+//         }
+
+//         return onestep;
+//     }
+// };
+
+
 int main(int argc, char const* argv[]) {
+    Solution s;
+    PrintListNode(s.middleNode(EvaluateListNode({1, 2, 3, 4, 5, 6})));
+
     return 0;
 }
--- linked-list/ReverseLinkedList.cpp
+++ linked-list/ReverseLinkedList.cpp
@@ -1,5 +1,24 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL;
+        ListNode *curr = NULL;
+
+        while(head != NULL) {
+            curr = head->next;
+            head->next = prev;
+            prev = head;
+            head = curr;
+        }
+        
+        return prev;
+    }
+};
+
 int main(int argc, char const* argv[]) {
+    Solution s;
+    PrintListNode(s.reverseList(EvaluateListNode({1, 2, 3})));
     return 0;
 }
