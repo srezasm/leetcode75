b0596332f221605379fa1359e8df3a1f749e9370
srezasm <srezas.2017@gmail.com>
2023-01-17 16:29:19 +0330

    linked list cycle II

--- linked-list/LinkedListCycleII.cpp
+++ linked-list/LinkedListCycleII.cpp
@@ -1,5 +1,31 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    ListNode* detectCycle(ListNode* head) {
+        if (!head || !head->next)
+            return nullptr;
+
+        ListNode* slow = head->next;
+        ListNode* fast = head->next->next;
+
+        while (slow != fast) {
+            slow = slow->next;
+            if (fast == nullptr || fast->next == nullptr)
+                return nullptr;
+            fast = fast->next->next;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
+    }
+};
+
 int main(int argc, char const* argv[]) {
     return 0;
 }
