3cbacc29363f995977d67a9e0e2b60a117646d9b
srezasm <srezas.2017@gmail.com>
2023-01-19 12:51:56 +0330

    greedy

--- greedy/BestTimetoBuyandSellStock.cpp
+++ greedy/BestTimetoBuyandSellStock.cpp
@@ -1,5 +1,23 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    int maxProfit(vector<int> prices) {
+        int least = INT_MAX;
+        int profit = 0;
+
+        for (int i = 0; i < prices.size(); i++) {
+            least = min(least, prices[i]);
+            profit = max(prices[i] - least, profit);
+        }
+
+        return profit;
+    }
+};
+
 int main(int argc, char const* argv[]) {
+    Solution s;
+    assert(s.maxProfit({7, 6, 4, 3, 1}) == 0);
+    assert(s.maxProfit({7, 1, 5, 3, 6, 4}) == 5);
     return 0;
 }
--- greedy/LongestPalindrome.cpp
+++ greedy/LongestPalindrome.cpp
@@ -1,5 +1,41 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> accuranceCount;
+        int result = 0;
+        int len = s.length();
+
+        if (len < 2)
+            return len;
+
+        for (int i = 0; i < len; i++) {
+            accuranceCount[s[i]]++;
+        }
+
+        for (auto c : accuranceCount) {
+            if (c.second % 2 == 0)
+                result += c.second;
+            else if (c.second > 2)
+                result += --c.second;
+        }
+
+        for (auto c : accuranceCount) {
+            if (c.second % 2 == 1) {
+                result++;
+                break;
+            }
+        }
+
+        return result;
+    }
+};
+
 int main(int argc, char const* argv[]) {
+    Solution s;
+    assert(s.longestPalindrome("abccccdd") == 7);   // dccbccd
+    assert(s.longestPalindrome("aaabbbcco") == 7);  // cabobac
+    assert(s.longestPalindrome("a") == 1);          // a
     return 0;
 }
