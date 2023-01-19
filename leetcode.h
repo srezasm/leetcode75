e2b57d4a17d3129169b57f1dde2bc7077741a882
srezasm <srezas.2017@gmail.com>
2023-01-19 12:59:46 +0330

    tree definition added to leetcode.h

--- leetcode.h
+++ leetcode.h
@@ -27,3 +27,27 @@ void PrintListNode(ListNode* head) {
     }
     cout << endl;
 }
+
+class Node {
+   public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) { val = _val; }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
