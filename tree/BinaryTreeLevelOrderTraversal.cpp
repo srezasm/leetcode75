19e68ab20f501ad5ef7eca5aa74103fa0b97f57a
srezasm <srezas.2017@gmail.com>
2023-01-21 14:31:52 +0330

    tree

--- tree/BinaryTreeLevelOrderTraversal.cpp
+++ tree/BinaryTreeLevelOrderTraversal.cpp
@@ -1,5 +1,53 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (root == nullptr)
+            return {};
+
+        vector<TreeNode*> queue;
+        vector<vector<int>> answer;
+
+        answer.push_back({root->val});
+
+        if (root->left)
+            queue.push_back(root->left);
+        if (root->right)
+            queue.push_back(root->right);
+
+        vector<int> currentAnswer;
+        while (!queue.empty()) {
+            currentAnswer.erase(begin(currentAnswer), end(currentAnswer));
+            int removalCout = 0;
+
+            int queueSize = queue.size();
+            for (int i = 0; i < queueSize; i++) {
+                TreeNode* item = queue[i];
+                if (item == nullptr)
+                    continue;
+
+                currentAnswer.push_back(item->val);
+
+                if (item->left)
+                    queue.push_back(item->left);
+                if (item->right)
+                    queue.push_back(item->right);
+
+                removalCout++;
+            }
+
+            for (int i = 0; i < removalCout; i++) {
+                queue.erase(begin(queue));
+            }
+
+            answer.push_back(currentAnswer);
+        }
+
+        return answer;
+    }
+};
+
 int main(int argc, char const* argv[]) {
     return 0;
 }
--- tree/N-aryTreePreorderTraversal.cpp
+++ tree/N-aryTreePreorderTraversal.cpp
@@ -1,5 +1,53 @@
 #include "../leetcode.h"
 
+// Runtime: 18 ms
+// Memory:  11.5 MB
+class Solution {
+   public:
+    vector<int> answer;
+    
+    void fillAnswer(Node* root) {
+        if(root == nullptr)
+            return;
+
+        answer.push_back(root->val);
+
+        for(auto child: root->children) {
+            fillAnswer(child);
+        }
+    }
+
+    vector<int> preorder(Node* root) {
+        fillAnswer(root);
+        return answer;
+    }
+};
+
+// Runtime: 32 ms
+// Memory:  14.3 MB
+// class Solution {
+//    public:
+//     vector<int> preorder(Node* root) {
+//         if(root == nullptr)
+//             return {};
+//         if (root->children.size() == 0) {
+//             return {root->val};
+//         }
+
+//         vector<int> result;
+//         result.push_back(root->val);
+//         for (auto child : root->children) {
+//             auto childResult = preorder(child);
+//             result.insert(end(result), begin(childResult), end(childResult));
+//         }
+
+//         return result;
+//     }
+// };
+
 int main(int argc, char const* argv[]) {
+    Solution s;
+    // Node x = NULL;
+    // s.preorder(&x);
     return 0;
 }
