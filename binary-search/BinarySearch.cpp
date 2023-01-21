10d9f8a56bb3139c6ca3544fd55e3feeb61b652f
srezasm <srezas.2017@gmail.com>
2023-01-21 21:22:48 +0330

    binary search

--- binary-search/BinarySearch.cpp
+++ binary-search/BinarySearch.cpp
@@ -1,5 +1,38 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        short int end = nums.size() - 1;
+        short int start = 0;
+        short int middle;
+
+        while (end >= start) {
+            middle = (start + end) / 2;
+            if (nums[middle] == target)
+                return middle;
+            else if (target < nums[middle]) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
 int main(int argc, char const* argv[]) {
+    Solution s;
+    vector<int> nums;
+
+    // nums = {-1, 0, 3, 5, 9, 12};
+    // assert(s.search(nums, 9) == 4);
+
+    // nums = {-1, 0, 3, 5, 9, 12};
+    // assert(s.search(nums, 2) == -1);
+
+    nums = {5};
+    assert(s.search(nums, 5) == 0);
     return 0;
 }
--- binary-search/FirstBadVersion.cpp
+++ binary-search/FirstBadVersion.cpp
@@ -1,5 +1,45 @@
 #include "../leetcode.h"
 
+bool isBadVersion(int version) {
+}
+
+class Solution {
+   public:
+    int firstBadVersion(int n) {
+        // long start = 1;
+        // long mid;
+        // while (true) {
+        //     mid = (start + n) / 2;
+        //     if (isBadVersion(mid)) {
+        //         mid = (start + mid - 1) / 2;
+        //         while (isBadVersion(mid)) {
+        //             mid = (start + mid - 1) / 2;
+        //         }
+        //         while (!isBadVersion(mid))
+        //             mid++;
+        //         return mid;
+        //     } else {
+        //         start = mid + 1;
+        //     }
+        // }
+        // return 1;
+
+        int start = 1;
+        int mid;
+
+        while(start < n) {
+            mid = start + (n - start) / 2;
+
+            if(!isBadVersion(mid))
+                start = mid + 1;
+            else
+                n = mid;
+        }
+
+        return start;
+    }
+};
+
 int main(int argc, char const* argv[]) {
     return 0;
 }
