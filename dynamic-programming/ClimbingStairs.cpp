8770d101f21734cc3e8c8a6dd072753887a0acd8
srezasm <srezas.2017@gmail.com>
2023-01-24 11:58:10 +0330

    dynamic programming

--- dynamic-programming/ClimbingStairs.cpp
+++ dynamic-programming/ClimbingStairs.cpp
@@ -1,5 +1,55 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    // ** Memoized recursive **
+    // unordered_map<int, int> memo;
+    // int climbStairs(int n) {
+    //     if (n == 0)
+    //         return 1;
+    //     if (n == -1)
+    //         return 0;
+
+    //     if (memo[n])
+    //         return memo[n];
+
+    //     int ans = climbStairs(n - 1) + climbStairs(n - 2);
+    //     memo[n] = ans;
+    //     return ans;
+    // }
+
+    // ** Normal (array) **
+    // int climbStairs(int n) {
+    //     vector<int> steps = {1, 1};
+
+    //     if (n < 2)
+    //         return steps[n];
+
+    //     for (int i = 2; i <= n; i++)
+    //         steps.push_back(steps[i - 1] + steps[i - 2]);
+
+    //     return steps[n];
+    // }
+
+    // ** Normal **
+    int climbStairs(int n) {
+        int step1 = 1;
+        int step2 = 1;
+        int result = 0;
+
+        if (n < 2)
+            return 1;
+
+        for (int i = 2; i <= n; i++) {
+            result = step1 + step2;
+            step1 = step2;
+            step2 = result;
+        }
+
+        return result;
+    }
+};
+
 int main(int argc, char const* argv[]) {
     return 0;
 }
--- dynamic-programming/FibonacciNumber.cpp
+++ dynamic-programming/FibonacciNumber.cpp
@@ -1,5 +1,23 @@
 #include "../leetcode.h"
 
+class Solution {
+   public:
+    unordered_map<int, int> commonMap;
+
+    int fib(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 0)
+            return 0;
+        if (commonMap[n])
+            return commonMap[n];
+
+        int ans = fib(n - 1) + fib(n - 2);
+        commonMap[n] = ans;
+        return ans;
+    }
+};
+
 int main(int argc, char const* argv[]) {
     return 0;
 }
